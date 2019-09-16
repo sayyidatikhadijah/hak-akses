18a21f0116a03236c490b9288c81abec9d705246
sayyidatikhadijah <42019496+sayyidatikhadijah@users.noreply.github.com>
2019-09-16 11:22:10 +0700

    hak-akses
    hak-akses

---
+++ Dashboard.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Dashboard extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('cek_login');
+    }
+    public function index()
+    {
+        return view('index');
+    }
+}
